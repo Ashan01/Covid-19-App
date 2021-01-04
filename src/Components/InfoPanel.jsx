9d1b05b61d7243a6982e638c6dfa8a962ef4968e
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-01-04 08:14:40 +0500

    add data via api

--- src/Components/InfoPanel.jsx
+++ src/Components/InfoPanel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
@@ -20,26 +20,46 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function InfoPanel() {
+   let [data_1, setData] = useState({});
+   useEffect(() => {
+      async function getData() {
+         try {
+            let response = await fetch("https://covid19.mathdro.id/api");
+            let json = await response.json();
+
+            delete json["dailySummary"];
+            delete json["dailyTimeSeries"];
+            delete json["dailySummary"];
+            delete json["image"];
+            delete json["countries"];
+            delete json["source"];
+            delete json["countryDetail"];
+            delete json["lastUpdate"];
+
+            setData(json);
+         } catch (error) {
+            console.log(error);
+         }
+      }
+      getData();
+   }, []);
    const classes = useStyles();
 
    return (
       <div className={classes.root}>
          <Grid container spacing={3} className={classes.Grids}>
-            <Grid item xs={4}>
-               <Paper className={classes.paper} elevation={3}>
-                  xs=12
-               </Paper>
-            </Grid>{" "}
-            <Grid item xs={4}>
-               <Paper className={classes.paper} elevation={3}>
-                  xs=12
-               </Paper>
-            </Grid>{" "}
-            <Grid item xs={4}>
-               <Paper className={classes.paper} elevation={3}>
-                  xs=12
-               </Paper>
-            </Grid>
+            {Object.keys(data_1).map((val, ind) => {
+               return (
+                  <Grid item xs={4} key={ind}>
+                     <Paper className={classes.paper} elevation={3}>
+                        <div>
+                           <h3>{val}</h3>
+                           <h3>{data_1[val].value}</h3>
+                        </div>
+                     </Paper>
+                  </Grid>
+               );
+            })}
          </Grid>
       </div>
    );
