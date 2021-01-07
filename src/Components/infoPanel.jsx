7bd7e18ad8a0c48440aa8a527a3cc398561d3493
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-01-08 03:24:00 +0500

    updated

--- src/Components/infoPanel.jsx
+++ src/Components/infoPanel.jsx
@@ -1,31 +1,66 @@
 import React, { useEffect, useState } from "react";
 import { NavBar } from "./navBar";
-i;
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+   root: {
+      flexGrow: 1,
+   },
+   paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+      margin: "50px",
+   },
+}));
 
 export const InfoPanel = () => {
    const classes = useStyles();
 
-   let [data, setData] = useState("");
+   let [data, setData] = useState({});
+
    useEffect(() => {
-      async function data() {
-         let res = await fetch("https://covid19.mathdro.id/api");
-         let jsonData = await res.json();
+      async function fetchData() {
+         try {
+            let res = await fetch("https://covid19.mathdro.id/api");
+            let jsonData = await res.json();
 
-         delete jsonData["dailySummary"];
-         delete jsonData["dailyTimeSeries"];
-         delete jsonData["image"];
-         delete jsonData["source"];
-         delete jsonData["countries"];
-         delete jsonData["countryDetail"];
-         delete jsonData["lastUpdate"];
+            delete jsonData["dailySummary"];
+            delete jsonData["dailyTimeSeries"];
+            delete jsonData["image"];
+            delete jsonData["source"];
+            delete jsonData["countries"];
+            delete jsonData["countryDetail"];
+            delete jsonData["lastUpdate"];
 
-         setData(jsonData);
+            setData(jsonData);
+         } catch (error) {
+            console.log(error);
+         }
       }
-      data();
+      fetchData();
    }, []);
+
    return (
       <div>
          <NavBar />
+
+         <div className={classes.root}>
+            <Grid container spacing={3}>
+               {Object.keys(data).map((obj, ind) => {
+                  return (
+                     <Grid item xs key={ind}>
+                        <Paper className={classes.paper} elevation={3}>
+                           <h3>{obj}</h3>
+                           <h3>{obj}.['value']</h3>
+                        </Paper>
+                     </Grid>
+                  );
+               })}
+            </Grid>
+         </div>
       </div>
    );
 };
